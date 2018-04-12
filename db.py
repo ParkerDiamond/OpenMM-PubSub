bd8fadac806abbc3d3ec38e46ced849e1d92ec6f
ParkerDiamond <jparkerdiamond@gmail.com>
2018-04-12 11:51:32 -0400

    Added backend DB schema; still need to link it

---
+++ db.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=False, nullable=False)
+    account_id = db.Column(db.Integer, unique=False, nullable=True)
+    jobs_submitted = db.Column(db.Integer, unique=False, nullable=False)
+    jobs_completed = db.Column(db.Integer, unique=False, nullable=False)
+    computer_rating = db.Column(db.Float, unique=False, nullable=True)
+
+class Jobs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    files = db.Column(db.String(80), unique=True, nullable=False)
+    est_hours = db.Column(db.Float, unique=False, nullable=False)
+    payout = db.Column(db.Float, unique=False, nullable=False)
